3bd208e241f176b9799069c0f6317333d9ac71f6
gitpriyajain <priya.j@glowtouch.com>
2022-06-06 18:58:33 +0530

    angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HelpComponent } from './help/help.component';
+import { TermsComponent } from './terms/terms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'help',component:HelpComponent},
+  {path:'home',component:HomeComponent}, 
+  {path:'terms',component:HelpComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';    
+import { NgModule } from '@angular/core';    
+import { AppRoutingModule } from './app-routing.module';    
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HelpComponent } from './help/help.component';
+import { TermsComponent } from './terms/terms.component';    
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({    
+  declarations: [    
+    AppComponent, HomeComponent, HelpComponent, TermsComponent,        
+  ],    
+  imports: [    
+    BrowserModule,    
+    AppRoutingModule,        
+  ],    
+  providers: [],    
+  bootstrap: [AppComponent]    
+})    
+
 export class AppModule { }
